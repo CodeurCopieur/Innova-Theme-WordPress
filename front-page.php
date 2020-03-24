2ee4fd5bac9de6f86fc9752051a4d8a0b002c835
codwerk <louis_widdy@live.fr>
2020-03-25 00:01:40 +0100

    add section testimonial

--- front-page.php
+++ front-page.php
@@ -78,7 +78,7 @@ $hero = get_field('hero');  ?>
                             <h2><?php echo $title; ?></h2>
                             <p><?php echo $categorie; ?></p>
                           </div>
-                          <img src="<?php echo $image['url']; ?>" alt="Image" class="img-fluid">
+                          <img src="<?php echo $image['sizes']['product_image_small']; ?>" alt="Image" class="img-fluid">
                         </a>
                       </div>
 
@@ -89,135 +89,61 @@ $hero = get_field('hero');  ?>
 
           <?php endif; ?>
 
-
-      <!--
-        <div class="col-sm-6 col-md-6 col-lg-6" data-aos="fade" data-aos-delay="100">
-          <a href="portfolio-single.html" class="work-thumb">
-            <div class="work-text">
-              <h2>Project Name Here</h2>
-              <p>Business</p>
-            </div>
-            <img src="<?php bloginfo('template_directory');?>/images/work_1.jpg" alt="Image" class="img-fluid">
-          </a>
-        </div>
-
-        <div class="col-sm-6 col-md-6 col-lg-6" data-aos="fade" data-aos-delay="200">
-          <a href="portfolio-single.html" class="work-thumb">
-            <div class="work-text">
-              <h2>Project Name Here</h2>
-              <p>Design</p>
-            </div>
-            <img src="<?php bloginfo('template_directory');?>/images/work_2.jpg" alt="Image" class="img-fluid">
-          </a>
-        </div>
-
-        <div class="col-sm-6 col-md-6 col-lg-6" data-aos="fade" data-aos-delay="300">
-          <a href="portfolio-single.html" class="work-thumb">
-            <div class="work-text">
-              <h2>Project Name Here</h2>
-              <p>Business</p>
-            </div>
-            <img src="<?php bloginfo('template_directory');?>/images/work_3.jpg" alt="Image" class="img-fluid">
-          </a>
-        </div>
-
-        <div class="col-sm-6 col-md-6 col-lg-6" data-aos="fade" data-aos-delay="400">
-          <a href="portfolio-single.html" class="work-thumb">
-            <div class="work-text">
-              <h2>Project Name Here</h2>
-              <p>Business</p>
-            </div>
-            <img src="<?php bloginfo('template_directory');?>/images/work_4.jpg" alt="Image" class="img-fluid">
-          </a>
-        </div>-->
-
-
   </div>
+
+ <?php $testimonial = get_field('testimonial');  ?>
 
   <div class="section bg-light block-11">
     <div class="container">
       <div class="row justify-content-center mb-5">
         <div class="col-md-8 text-center">
-          <h2 class="mb-4 section-title">Happy Client</h2>
+          <h2 class="mb-4 section-title"><?php echo $testimonial['medium_title']; ?></h2>
         </div>
       </div>
-      <div class="nonloop-block-11 owl-carousel">
-        <div class="item">
+
+
+     <!-- <div class="nonloop-block-11 owl-carousel"> -->
+
+      <?php if( have_rows('client') ): ?>
+
+        <div class="nonloop-block-11 owl-carousel">
+        
+
+      <?php while( have_rows('client') ): the_row(); 
+
+      // vars
+      $image = get_sub_field('image_client');
+      $nom = get_sub_field('nom_client');
+      $metier = get_sub_field('metier_client');
+      $text = get_sub_field('contenu');
+
+      ?>
+      
+      <div class="item">
           <div class="block-33 h-100">
             <div class="vcard d-flex mb-3">
-              <div class="image align-self-center"><img src="<?php bloginfo('template_directory');?>/images/person_1.jpg" alt="Person here"></div>
+              <div class="image align-self-center"><img src="<?php echo $image['sizes']['product_image_small']; ?>" alt="Person here"></div>
               <div class="name-text align-self-center">
-                <h2 class="heading">Carl Smith</h2>
-                <span class="meta">Customer Corp.</span>
+                <h2 class="heading"><?php echo $nom; ?></h2>
+                <span class="meta"><?php echo $metier; ?></span>
               </div>
             </div>
             <div class="text">
               <blockquote>
-                <p>&rdquo; Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat qui, ipsum debitis id
-                  reprehenderit ut soluta possimus consectetur asperiores totam veniam quasi molestiae. Animi doloribus
-                  accusamus eius ex laudantium vitae? &ldquo;</p>
+               <!-- <p>&rdquo; <?php echo $text; ?> &ldquo;</p>-->
+               <p><?php echo $text; ?></p> 
               </blockquote>
             </div>
           </div>
-        </div>
+          </div> 
 
-        <div class="item">
-          <div class="block-33 h-100">
-            <div class="vcard d-flex mb-3">
-              <div class="image align-self-center"><img src="<?php bloginfo('template_directory');?>/images/person_2.jpg" alt="Person here"></div>
-              <div class="name-text align-self-center">
-                <h2 class="heading">Craig Darren</h2>
-                <span class="meta">Customer Corp.</span>
-              </div>
-            </div>
-            <div class="text">
-              <blockquote>
-                <p>&rdquo; Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, ut enim error molestias maxime
-                  deserunt voluptate doloribus nihil nesciunt id perferendis, doloremque earum architecto ab incidunt
-                  sunt
-                  corrupti. Consequuntur qui ullam voluptates sapiente quod esse eum. &ldquo;</p>
-              </blockquote>
-            </div>
+        <?php endwhile; ?>
           </div>
-        </div>
+          
+      <?php endif; ?>
 
-        <div class="item">
-          <div class="block-33 h-100">
-            <div class="vcard d-flex mb-3">
-              <div class="image align-self-center"><img src="<?php bloginfo('template_directory');?>/images/person_1.jpg" alt="Person here"></div>
-              <div class="name-text align-self-center">
-                <h2 class="heading">John Smith</h2>
-                <span class="meta">Customer Corp.</span>
-              </div>
-            </div>
-            <div class="text">
-              <blockquote>
-                <p>&rdquo; Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, ab? Asperiores dolore
-                  blanditiis quis sapiente soluta aliquid officiis. &ldquo;</p>
-              </blockquote>
-            </div>
-          </div>
-        </div>
+      
 
-        <div class="item">
-          <div class="block-33 h-100">
-            <div class="vcard d-flex mb-3">
-              <div class="image align-self-center"><img src="<?php bloginfo('template_directory');?>/images/person_2.jpg" alt="Person here"></div>
-              <div class="name-text align-self-center">
-                <h2 class="heading">John Smith</h2>
-                <span class="meta">Customer Corp.</span>
-              </div>
-            </div>
-            <div class="text">
-              <blockquote>
-                <p>&rdquo; Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, accusamus? Porro fugit culpa
-                  consequuntur dolorum. &ldquo;</p>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-
-      </div>
     </div>
   </div>
   <!-- END .block-4 -->
@@ -254,10 +180,10 @@ $hero = get_field('hero');  ?>
     <div class="container text-center">
       <div class="row justify-content-center">
         <div class="col-lg-7">
-          <h3 class="text-white mb-2 font-weight-normal" >Get Started</h3>
+          <h3 class="text-white mb-2 font-weight-normal">Contactez-nous !</h3>
           <p class="text-white mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             
-          <p class="mb-0" data-aos="fade-up" data-aos-delay="200"><a href="contact.html" class="btn btn-outline-white px-4 py-3">Get In Touch!</a></p>
+          <p class="mb-0" data-aos="fade-up" data-aos-delay="200"><a href="contact.html" class="btn btn-outline-white px-4 py-3">Contactez-nous !</a></p>
         </div>
       </div>
 
